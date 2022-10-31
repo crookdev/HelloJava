0340d4b4112d2099c85312974fbc593c31ef0e11
crookdev <72733909+crookdev@users.noreply.github.com>
2022-10-31 21:09:59 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!  this is update 1";
+    private final String message = "Hello World!  this is update 2";
 
     public App() {}
 
